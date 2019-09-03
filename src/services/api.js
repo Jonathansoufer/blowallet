af503073c8f2e2d4706bef707d09734d643984f2
Jonathan Ferreira <jonathanferreira23@gmail.com>
2019-09-03 02:45:12 +0100

    Axios removed feature

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL:
-    'https://gist.githubusercontent.com/AbdoulNdiaye/b253a0a7b093cd5e775c85261780cff5/',
-});
-
-export default api;
